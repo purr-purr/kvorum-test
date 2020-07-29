22c0ae00502a7648109792f95f9f1cf4d9fc1c90
Tony <56505654+666hesoyam@users.noreply.github.com>
2020-07-29 12:27:34 +0300

    Add files via upload

---
+++ HTML/js/main.js
@@ -0,0 +1,7 @@
+
+//  <!-- Скрипт скрытия уведомления .alert , при нажатии на кнопку .alert__close -->
+        $(document).ready(function(){
+            $(".alert__close").click(function(){
+                $(".alert").toggleClass("hide"); return false;
+            });
+        });;
