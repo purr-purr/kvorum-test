0a3fbaa4c7660cf54c4a7e7807072990468b3e77
666hesoyam <56505654+666hesoyam@users.noreply.github.com>
2020-08-04 16:28:28 +0300

    cleen css

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,5 +1,5 @@
 
-//  <!-- Скрипт скрытия уведомления .alert , при нажатии на кнопку .alert__close -->
+//  Скрипт скрытия уведомления .alert , при нажатии на кнопку .alert__close
         $(document).ready(function(){
             $(".alert__close").click(function(){
                 $(".alert").toggleClass("hide"); return false;
@@ -7,41 +7,7 @@
         });;
 
 
-
-function print_doc(){
-window.print() ;
-}
-
-
-
-// $(function() { 
-//     var reCaptchaWidth = 302;
-//     var containerWidth = $('.recaptcha').width(); 
-//     if(reCaptchaWidth > containerWidth) {
-//         var reCaptchaScale = containerWidth / reCaptchaWidth;
-//         $('.g-recaptcha').css({
-//             'transform':'scale('+reCaptchaScale+')',
-//             'transform-origin':'left top'
-//         });
-//     }                
-// });  
-
-
-// function changeCapthaSize() {
-//     var reCaptchaWidth = 302;
-//     var containerWidth = $('.recaptcha').width();
-//     if(reCaptchaWidth > containerWidth) {
-//         var reCaptchaScale = containerWidth / reCaptchaWidth;
-//         $('.g-recaptcha').css({
-//             'transform':'scale('+reCaptchaScale+')',
-//             'transform-origin':'center top'
-//             });
-// }
-// };
-
-// $(window).ready(function(){
-// changeCapthaSize();
-// });
-// $(window).resize(function(){
-// changeCapthaSize();
-// });
+// Печать при нажатии на кнопку
+        function print_doc(){
+        window.print() ;
+        }
